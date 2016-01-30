716a2ec860a42dcb1379ff5b2f5497aec3003af1
Carter Rhea <crhea@g.cofc.edu>
2016-01-30 14:00:06 -0500

    hydro.py

--- hydro.py
+++ hydro.py
@@ -8,16 +8,16 @@ Created on Fri Jan 29 22:16:18 2016
 import numpy as np
 import matplotlib.pyplot as plt
 import time as t
-def time_step(s,y,a,b,n):
+def time_step(x_length,y_length,y,a,b,n):
     #y[0,0] = y_init
     
     #y[10,10] = .0001
     h = (b-a)/n
     t_0=a
-    for i in range(0,s):
+    for i in range(0,n):
         t_i = t_0+i*h
-        for i in range(0,s-1):
-            for j in range(0,s-1):
+        for i in range(0,y_length-1):
+            for j in range(0,x_length-1):
                 y[i,j] = t_i*(y[i+1,j]-4*y[i,j]+y[i-1,j]+y[i,j+1]+y[i,j-1])+y[i,j]
     return y
 def contour_plot(n,field):
@@ -31,11 +31,13 @@ def contour_plot(n,field):
    
 def main():
     s=20
-    y = np.matrix(np.zeros((s,s)))
-    
-    y[5,5] = 100
+    x_length = 280
+    y_length = 160
+    y = np.matrix(np.zeros((y_length,x_length)))
+    y[15,15] = 10
+    y[10,10] = 10
     #for i in range(0,1000):
-    scalar_field = time_step(s,y,0,10,1000)
+    scalar_field = time_step(x_length,y_length,y,0,10,1000)
     scalar_field = np.array(scalar_field)
     contour_plot(s,scalar_field)
         #t.sleep(1)
